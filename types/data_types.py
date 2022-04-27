217799faf6638251ca4695f114c42466098c04c1
Tarek Elgindy <tarek.elgindy@nrel.gov>
2022-04-27 17:06:12 -0600

    initial consolidation of data types

--- types/data_types.py
+++ types/data_types.py
@@ -1,145 +1,70 @@
 import datetime
 from enum import Enum
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List,Optional
 
 ### Supporting Functions ###
+# TODO: Connect with CIM values
 
 
 class Complex(BaseModel):
     real: float
     imag: float
 
+class StateArray(BaseModel):
+    values: List[int]
+    ids: List[str]
+    state_type: StateType
+    time: Optional[datetime.time]
+
+class StateType(str, Enum):
+    switch = "switch"
+    capacitor = "capacitor"
+    regulator = "regulator"
+
+class CostArray(BaseModel):
+    values: List[List[float]]
+    ids: List[str]
+    cost_type: CostType
+    units: str
+    time: Optional[datetime.time]
+
+class CostType(BaseModel):
+    real_cost_function: "real_cost_function"
+    reactive_cost_function: "reactive_cost_function"
+    real_wholesale_prices: "real_wholesale_prices"
+    reactive_wholesale_prices: "reactive_wholesale_prices"
+    operational_cost: "operational_cost"
+    
+class MeasurementArray(BaseModel):
+    values: List[float]
+    ids: List[str]
+    units: str
+    measurement_type: MeasurementType
+    time: Optional[datetime.time]
 
 class MeasurementType(str, Enum):
     voltage_magnitude = "voltage_magnitude"
     voltage_angle = "voltage_angle"
+    voltage_real = "voltage_real"
+    voltage_imaginary = "voltage_imaginary"
     current_magnitude = "current_magnitude"
     current_angle = "current_angle"
+    current_real = "current_real"
+    current_imaginary = "current_imaginary"
+    power_magnitude = "power_magnitude"
+    power_angle = "power_angle"
+    power_imaginary = "power_imaginary"
+    power_magnitude = "power_magnitude"
+    state_of_charge = "state_of_charge"
 
-
-class TimeseriesValue(BaseModel):
-    time: datetime.time
-    value: float
-
-
-class MeasurementModel(BaseModel):
-    measurement_type: MeasurementType = Field(None, alias="Measurement_Type")
-    is_flow: bool
-    standard_deviation: float
-
+class Topology(BaseModel):
+    admittance_matrix: AdmittanceMatrix
+    base_voltage_angles = MeasurementArray
+    base_voltage_magnitudes = MeasurementArray
+    slack_bus: str
 
 class AdmittanceMatrix(BaseModel):
-    "CIM_NAME:"
-    data: List[List[Complex]]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": ""}
-
-
-class RegulatorSteps(BaseModel):
-    "CIM_NAME:"
-    data: List[int]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": ""}
-
-
-class SwitchStates(BaseModel):
-    data: List[bool]
-    label: List[str]
-
-
-class CapacitorStates(BaseModel):
-    data: List[bool]
-    label: List[str]
-
-
-class MeasurementModels(BaseModel):
-    data: List[MeasurementModel]
-    label: List[str]
-
-
-class TimeseriesValues(BaseModel):
-    data: List[TimeseriesValue]
-    label: List[str]
-
-
-class BusVoltages(BaseModel):
-    data: List[Complex]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "kV"}
-
-
-class LineFlows(BaseModel):
-    data: List[Complex]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "Amps"}
-
-
-class PowerData(BaseModel):
-    data: List[Complex]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "kVA"}
-
-
-class RealCostFunctions(BaseModel):
-    data: List[List[float]]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "$/kWh"}
-
-
-class ReactiveCostFunctions(BaseModel):
-    data: List[List[float]]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "$/kVarh"}
-
-
-class OperationalCosts(BaseModel):
-    data: List[float]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "$"}
-
-
-class RealWholesalePrices(BaseModel):
-    data: List[TimeseriesValue]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "$/kWh"}
-
-
-class ReactiveWholesalePrices(BaseModel):
-    data: List[TimeseriesValue]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "$/kVarh"}
-
-
-class StateOfCharge(BaseModel):
-    data: List[float]
-    label: List[str]
-
-    class Config:
-        schema_extra = {"units": "%"}
-
-
-# print(Matrix.schema_json(indent=2))
-# print(Measurement_Model.schema_json(indent=2))
-# test = Matrix(data=[[Complex(real=1,imag=2),Complex(real=2,imag=3)],[Complex(real=1,imag=2),Complex(real=4,imag=2)]],label=['a','b'])
-# test2 = Measurement_Model( measurement_type = 'voltage_angle',is_flow =True,standard_deviation=0)
+    values: List[List[Complex]]
+    ids: List[str]
+    units: str
