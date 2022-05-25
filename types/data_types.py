cfbb585f8c2d349d33160dd95cb2d550247c6a17
Tarek Elgindy <tarek.elgindy@nrel.gov>
2022-05-24 19:13:24 -0600

    adding unit defaults and adjusting injection bools

--- types/data_types.py
+++ types/data_types.py
@@ -43,7 +43,7 @@ class CostArray(BaseModel):
     """
     values: List[List[float]]
     ids: List[str]
-    units: str
+    units: str = '$'
     time: Optional[datetime.time]
 
 class RealCostFunctions(CostArray):
@@ -85,57 +85,76 @@ class MeasurementArray(BaseModel):
     values: List[float]
     ids: List[str]
     units: str
-    injection: Optional[List[bool]]
     accuracy: Optional[List[float]]
     bad_data_threshold: Optional[List[float]]
     time: Optional[datetime.time]
 
 class VoltagesMagnitude(MeasurementArray):
+    units: str = 'kV'
     pass
 
 class VoltagesAngle(MeasurementArray):
+    units: str = 'degrees'
     pass
 
 class VoltagesReal(MeasurementArray):
+    units: str = 'kV'
     pass
 
 class VoltagesImaginary(MeasurementArray):
+    units: str = 'kV'
     pass
 
 class CurrentsMagnitude(MeasurementArray):
+    units: str = 'A'
     pass
 
 class CurrentsAngle(MeasurementArray):
+    units: str = 'degrees'
     pass
 
 class CurrentsReal(MeasurementArray):
+    units: str = 'A'
     pass
 
 class CurrentsImaginary(MeasurementArray):
+    units: str = 'A'
     pass
 
 class PowersMagnitude(MeasurementArray):
+    injection: List[bool]
+    units str = 'kVA'
     pass
 
 class PowersAngle(MeasurementArray):
+    injection: List[bool]
+    units: str = 'degrees'
     pass
 
 class PowersReal(MeasurementArray):
+    injection: List[bool]
+    units: str = 'kW'
     pass
 
 class PowersImaginary(MeasurementArray):
+    injection: List[bool]
+    units: str = 'kVAR'
     pass
 
 class SolarIrradiances(MeasurementArray):
+    units: str = 'W/m^2'
     pass
 
 class Temperatures(MeasurementArray):
+    units: str = 'C'
     pass
 
 class WindSpeeds(MeasurementArray):
+    units: str = 'm/s'
     pass
 
 class StatesOfCharge(MeasurementArray):
+    units: str = 'percent'
     pass
 
 
@@ -143,16 +162,16 @@ class Topology(BaseModel):
     admittance:  Union[AdmittanceSparse, AdmittanceMatrix]
     base_voltage_angles = Optional[VoltagesAngle]
     base_voltage_magnitudes = Optional[VoltagesMagnitude]
-    slack_bus: List[str]
+    slack_bus: List[str] = []
 
 class AdmittanceSparse(BaseModel):
     admittance_list: List[Complex]
     from_equipment: List[str]    
     to_equipment:    List[str]    
     equipment_type: Optional[List[str]]
-    units = str
+    units = str = 'S'
 
 class AdmittanceMatrix(BaseModel):
     admittance_matrix: List[List[Complex]]
     ids: List[str]
-    units: str
+    units: str = 'S'
