d66df7f418f71c1e63a6b5f256bea21a7ccfa26a
Tarek Elgindy <tarek.elgindy@nrel.gov>
2022-07-22 17:03:26 -0600

    exporting schema for each object

--- gadal/gadal_types/generate_schema.py
+++ gadal/gadal_types/generate_schema.py
@@ -1,15 +1,15 @@
 from data_types import *
+import os
+
 all_classes = [StateArray, SwitchStates, CapacitorStates, RegulatorStates, CostArray, RealCostFunctions, ReactiveCostFunctions, RealWholesalePrices, ReactiveWholesalePrices, OperationalCosts, MeasurementArray, VoltagesMagnitude, VoltagesAngle, VoltagesReal, VoltagesImaginary, CurrentsMagnitude, CurrentsAngle, CurrentsReal, CurrentsImaginary, PowersMagnitude, PowersAngle, PowersReal, PowersImaginary, PowersImaginary, SolarIrradiances, Temperatures, WindSpeeds, StatesOfCharge, Topology, AdmittanceSparse, AdmittanceMatrix]
 
 all_data = ''
 
+if not os.path.exists('schemas'):
+    os.makedirs('schemas')
 for klass in all_classes:
     json_data = klass.schema_json(indent=4)
-    #json_data = json_data.replace('\\n','\n')
-    all_data += json_data
-    all_data+='\n'
-
-with open('data_types.json','w') as fp:
-    fp.write(all_data)
+    with open(os.path.join('schemas',klass.__name__+'.json'),'w') as fp:
+        fp.write(json_data)
 
 
