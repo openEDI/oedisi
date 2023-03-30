98528d46a08e8ebe54745a88d5fc935eca877728
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2023-03-30 13:09:51 -0600

    Bump version to fix version numbering

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ description = (
 
 setup(
     name="gadal",
-    version="0.2.2",
+    version="0.2.4",
     description=description,
     # long_description=readme,
     author="Joseph McKinsey",
