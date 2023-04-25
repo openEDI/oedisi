68fbe3894f514b94fd25c25ce6c07628764ecfd6
Latif <aadil.latif@gmail.com>
2023-04-25 01:27:29 -0600

    updating mapping

--- gadal/gadal_types/mapped_federates.py
+++ gadal/gadal_types/mapped_federates.py
@@ -3,7 +3,9 @@ from enum import IntEnum, Enum
 class AppPort(IntEnum):
     feeder =  8765
     measuring_federate = 8766
+    wls_federate = 8767
 
 class AppIP(Enum):
     feeder =  '192.168.0.1'
     measuring_federate = '192.168.0.2'
+    wls_federate = '192.168.0.3'
