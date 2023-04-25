dce6dfe767db4777da63bd9b9299a1131a631991
Latif <aadil.latif@gmail.com>
2023-04-25 00:49:36 -0600

    minor edit

--- gadal/gadal_types/mapped_federates.py
+++ gadal/gadal_types/mapped_federates.py
@@ -2,6 +2,8 @@ from enum import IntEnum, Enum
 
 class AppPort(IntEnum):
     feeder =  8765
+    measuring_federate = 8766
 
 class AppIP(Enum):
     feeder =  '192.168.0.1'
+    measuring_federate = '192.168.0.2'
