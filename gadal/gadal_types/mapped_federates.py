023fc0c2262e9368c63c1ca186c9a7d558f3f801
Latif <aadil.latif@gmail.com>
2023-04-25 01:43:53 -0600

    mapping update

--- gadal/gadal_types/mapped_federates.py
+++ gadal/gadal_types/mapped_federates.py
@@ -4,8 +4,10 @@ class AppPort(IntEnum):
     feeder =  8765
     measuring_federate = 8766
     wls_federate = 8767
+    recorder = 8768
 
 class AppIP(Enum):
     feeder =  '192.168.0.1'
     measuring_federate = '192.168.0.2'
     wls_federate = '192.168.0.3'
+    recorder = '192.168.0.4'
