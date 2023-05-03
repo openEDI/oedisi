0c25d87e9fad5729b02cef2f00cb45968681bb7c
Latif <alatif@nrel.gov>
2023-05-03 09:21:31 -0600

    port for broker

--- gadal/gadal_types/mapped_federates.py
+++ gadal/gadal_types/mapped_federates.py
@@ -5,9 +5,11 @@ class AppPort(IntEnum):
     measuring_federate = 8766
     wls_federate = 8767
     recorder = 8768
+    broker = 8769
 
 class AppIP(Enum):
     feeder =  '192.168.0.1'
     measuring_federate = '192.168.0.2'
     wls_federate = '192.168.0.3'
     recorder = '192.168.0.4'
+    broker = '192.168.0.5'
