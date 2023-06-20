f45cebe3b26b590c87be0813cfdb460ebc76dba4
Latif <alatif@nrel.gov>
2023-06-20 11:22:36 -0600

    addign services

--- oedisi/types/common.py
+++ oedisi/types/common.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-
+from typingDict import Dict
 
 class BrokerConfig(BaseModel):
     broker_port : int = 23404
     broker_ip : str = "10.5.0.2"
     api_port : int = 12345
+    services : Dict = {}
