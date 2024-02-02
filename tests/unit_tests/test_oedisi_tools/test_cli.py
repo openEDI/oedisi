10851270b327cfc6eb038252fb6fb84891e12fca
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2024-02-02 12:29:47 -0700

    Test one more

--- tests/unit_tests/test_oedisi_tools/test_cli.py
+++ tests/unit_tests/test_oedisi_tools/test_cli.py
@@ -28,7 +28,6 @@ def test_debug(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     assert result.exit_code == 0
 
 
-@pytest.mark.skip(reason="Testing")
 def test_build_description(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path)
 
