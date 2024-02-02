e6079fe99508893e1e3e1afb022f14908b77b0f6
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2024-02-02 12:11:43 -0700

    Add sss.s instead of sss.

--- tests/unit_tests/test_oedisi_tools/test_cli.py
+++ tests/unit_tests/test_oedisi_tools/test_cli.py
@@ -30,7 +30,6 @@ def test_debug(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     assert result.exit_code == 0
 
 
-@pytest.mark.skip(reason="Testing")
 def test_build_description(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path)
 
@@ -48,6 +47,7 @@ def test_build_description(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     assert result.exit_code == 0
 
 
+@pytest.mark.skip(reason="Testing")
 def test_bad_build_description(base_path: Path, monkeypatch: pytest.MonkeyPatch):
     monkeypatch.chdir(base_path)
 
