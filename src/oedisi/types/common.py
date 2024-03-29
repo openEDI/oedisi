6c786aafbdb7a8d612e7dfe0cc1b380aeadd0e8b
Latif <aadil.latif@gmail.com>
2024-03-29 10:48:21 -0600

    minor edit to BrokerConfig model

--- src/oedisi/types/common.py
+++ src/oedisi/types/common.py
@@ -16,7 +16,9 @@ class BrokerConfig(BaseModel):
     broker_port: int = 23404
     broker_ip: str = "10.5.0.2"
     api_port: int = 12345
-    services: Dict = {}
+    feeder_host: str
+    feeder_port: int
+    
 
 class HeathCheck(BaseModel):
     hostname: str
