7cc68491f0c93101ae61861320c07dd866349489
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2024-04-17 09:08:18 -0600

    Make feeder port and feeder hostname optional

--- src/oedisi/types/common.py
+++ src/oedisi/types/common.py
@@ -16,9 +16,9 @@ class BrokerConfig(BaseModel):
     broker_port: int = 23404
     broker_ip: str = "10.5.0.2"
     api_port: int = 12345
-    feeder_host: str
-    feeder_port: int
-    
+    feeder_host: str | None = None
+    feeder_port: int | None = None
+
 
 class HeathCheck(BaseModel):
     hostname: str
